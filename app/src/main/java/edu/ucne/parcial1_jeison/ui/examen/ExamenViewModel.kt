e286b289c48992d1c8047300ffc0ee2fc77a51d7
JeisonR23 <jeisonreyes001@gmail.com>
2022-09-22 19:22:00 -0400

    creando el viewModel

---
+++ app/src/main/java/edu/ucne/parcial1_jeison/ui/examen/ExamenViewModel.kt
@@ -0,0 +1,20 @@
+package edu.ucne.parcial1_jeison.ui.examen
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.parcial1_jeison.model.Examen
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExamenViewModel @Inject constructor(
+) : ViewModel(){
+
+    fun Guardar(){
+    }
+}
