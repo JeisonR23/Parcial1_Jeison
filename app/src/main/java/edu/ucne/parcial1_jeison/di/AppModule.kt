a3637ee79464a767e4a1b7782b6590a11d2c3d84
JeisonR23 <jeisonreyes001@gmail.com>
2022-09-22 19:24:09 -0400

    creando la clase AppModule

---
+++ app/src/main/java/edu/ucne/parcial1_jeison/di/AppModule.kt
@@ -0,0 +1,29 @@
+package edu.ucne.parcial1_jeison.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.parcial1_jeison.data.ExamenBd
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun ProvideTicketDb(@ApplicationContext context: Context): ExamenBd {
+        val DATABASE_NAME = "examenBd"
+        return Room.databaseBuilder(
+            context,
+            ExamenBd::class.java,
+            DATABASE_NAME       )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+
+}
