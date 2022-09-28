7b7b712a6122d4deabe04b7250dfeaf4d7454be6
JeisonR23 <jeisonreyes001@gmail.com>
2022-09-28 19:33:42 -0400

    Modificando la Clase Screen.kt

--- app/src/main/java/edu/ucne/parcial1_jeison/navegation/Screen.kt
+++ app/src/main/java/edu/ucne/parcial1_jeison/navegation/Screen.kt
@@ -2,7 +2,7 @@ package edu.ucne.parcial1_jeison.navegation
 
 sealed class Screen (val route : String){
 
-    object RegistroArticuloScreen : Screen("RegistroExamen")
-    object ConsultaArticuloScreen : Screen("ConsultaExamen")
+    object RegistroArticuloScreen : Screen("RegistroArticulo")
+    object ConsultaArticuloScreen : Screen("ConsultaArticulo")
 }
 
