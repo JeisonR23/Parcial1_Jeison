c832e347009d3962a2a48eacc1ce60819150f5df
JeisonR23 <jeisonreyes001@gmail.com>
2022-09-28 18:52:33 -0400

    Creando los Campos del registro

--- app/src/main/java/edu/ucne/parcial1_jeison/ui/examen/RegistroArticuloScreen.kt
+++ app/src/main/java/edu/ucne/parcial1_jeison/ui/examen/RegistroArticuloScreen.kt
@@ -1,8 +1,12 @@
 package edu.ucne.parcial1_jeison.ui.examen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
@@ -11,8 +15,27 @@ fun RegistroArticuloScreen(
     navHostController: NavHostController,
     viewModel: ArticuloViewModel = hiltViewModel()
 ) {
+    Column(modifier = Modifier.fillMaxWidth()) {
 
-    
+        OutlinedTextField(
+            value = viewModel.descripcion
+            , onValueChange ={viewModel.descripcion = it},
+         label = { Text(text = "Descripcion")},
+        modifier = Modifier.fillMaxWidth())
+    }
+    OutlinedTextField(
+        value = viewModel.marca
+        , onValueChange ={viewModel.marca = it},
+        label = { Text(text = "Marca")},
+        modifier = Modifier.fillMaxWidth())
+
+    OutlinedTextField(
+        value = viewModel.existencia
+        , onValueChange ={viewModel.existencia = it},
+        label = { Text(text = "Existencia")},
+        modifier = Modifier.fillMaxWidth())
+
+
 
     OutlinedButton(onClick = {
         viewModel.Guardar()
