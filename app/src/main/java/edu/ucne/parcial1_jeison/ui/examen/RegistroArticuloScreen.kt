d8a6792763f61c29c43f75ecaecd6e27c03e4dbf
JeisonR23 <jeisonreyes001@gmail.com>
2022-09-28 19:40:17 -0400

    Modificando los impots de la funcion RegistroArticuloScreen.kt

--- app/src/main/java/edu/ucne/parcial1_jeison/ui/examen/RegistroArticuloScreen.kt
+++ app/src/main/java/edu/ucne/parcial1_jeison/ui/examen/RegistroArticuloScreen.kt
@@ -4,15 +4,12 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.text.isDigitsOnly
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import edu.ucne.parcial1_jeison.navegation.Screen
 
 @Composable
 fun RegistroArticuloScreen(
